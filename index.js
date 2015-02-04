7399809747f82f7f059f586d89b00cb81eedf704
Alexander Gugel <alexander.gugel@gmail.com>
2015-02-05 00:44:06 +0100

    Add video

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 // http://left-shark.github.io/left-shark/left-shark.gif
 (function() {
-    
+    var allElements = document.querySelectorAll('*');
+
+    requestAnimationFrame(function loop(t) {
+        // console.log(t);
+
+        // requestAnimationFrame(loop);
+    });
+
+    var html = ('<div style="opacity: 0;">' +
+        '<iframe src="https://youtube.com/embed/MAtCSio876c?autoplay=1&controls=0&showinfo=0&autohide=1">' +
+        '</iframe>' +
+    '</div>');
+
+    var container = document.createElement('div');
+    container.innerHTML = html;
+    document.body.appendChild(container);
+
+    return true;
 }());
