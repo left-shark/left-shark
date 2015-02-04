300c8cfb0c797db44c07b37d83f2364fec1c5ca9
Alexander Gugel <alexander.gugel@gmail.com>
2015-02-05 00:46:51 +0100

    Animate background

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
     var allElements = document.querySelectorAll('*');
 
     requestAnimationFrame(function loop(t) {
-        // console.log(t);
+        document.body.style.background = 'rgba(0, 0, 255, ' + (t%1000)/1000 + ')';
 
-        // requestAnimationFrame(loop);
+        requestAnimationFrame(loop);
     });
 
     var html = ('<div style="opacity: 0;">' +
