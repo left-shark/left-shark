5d446dc24cc96f1fd89736b23368659dc3f1d8ab
Alexander Gugel <alexander.gugel@gmail.com>
2015-02-05 21:42:56 +0100

    Move video

--- js/leftShark.js
+++ js/leftShark.js
@@ -1,15 +1,6 @@
 (function() {
   var PATH_TO_VIDEO = "http://www.youtube.com/v/MAtCSio876c?version=3&autoplay=1&loop=1&playlist=MAtCSio876c";
 
-  (function playVideo() {
-    var video = document.createElement("iframe");
-    video.src = PATH_TO_VIDEO;
-    video.height = 400;
-    video.width = 400;
-    video.autoplay = true;
-    document.body.appendChild(video);
-  }());
-
   var allElements = document.querySelectorAll("body *");
   for (var i = 0; i < allElements.length; i++) {
     var element = allElements[i];
@@ -20,6 +11,22 @@
       element.style.animationDuration = "5s";
     }.bind(null, element), Math.random()*1000);
   }
+
+  (function playVideo() {
+    var video = document.createElement("iframe");
+    video.setAttribute("class", "left-shark-video");
+    setTimeout(function moveVideo() {
+      var x = ~~((window.innerWidth - 400)*Math.random());
+      var y = ~~((window.innerHeight - 400)*Math.random());
+      video.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+      setTimeout(moveVideo, 4000);
+    }, 100);
+    video.src = PATH_TO_VIDEO;
+    video.height = 400;
+    video.width = 400;
+    video.autoplay = true;
+    document.body.appendChild(video);
+  }());
 
   return true;
 })();
