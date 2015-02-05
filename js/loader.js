a822bb5d248099bd583b4337d4baa1100df586d1
Alexander Gugel <alexander.gugel@gmail.com>
2015-02-06 00:38:38 +0100

    Fix caching

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,13 @@
 (function() {
+    var rand = ~~(Math.random()*10000000);
     var scriptTag = document.createElement('script');
-    scriptTag.src = 'SCRIPT_HERE';
+    scriptTag.src = 'https://left-shark.github.io/left-shark/js/leftShark.js?' + rand;
     document.body.appendChild(scriptTag);
 
     var linkTag = document.createElement('link');
     linkTag.setAttribute('rel', 'stylesheet');
     linkTag.setAttribute('type', 'text/css');
-    linkTag.setAttribute('href', 'CSS_HERE');
+    linkTag.setAttribute('href', 'https://left-shark.github.io/left-shark/css/leftShark.css?' + rand);
     document.head.appendChild(linkTag);
     return true;
 }());
